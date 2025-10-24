dbd2fa76685ef2819b2f1c6a0776c3e9d639f3b8
Sergio Pérez <sergio30po@gmail.com>
2025-10-24 13:12:27 +0200

    Update DESCRIPTION and add CITATION file

---
+++ R/miRPM-package.R
@@ -0,0 +1,16 @@
+#' miRPM: miRNA RPM Normalization
+#'
+#' Functions for the analysis of count matrices and RPM normalization in miRNA studies.
+#'
+#' This package provides lightweight tools to explore, normalize, and analyze
+#' miRNA-seq count matrices using reads per million (RPM) normalization.
+#'
+#' @author
+#' Sergio Pérez Oliveira – sergio30po@gmail.com
+#'
+#' Manuel Fernández Sanjurjo – fernandezsanmanuel@uniovi.es
+#'
+#' Eduardo Iglesias Gutierrez – iglesiaseduardo@uniovi.es
+#'
+"_PACKAGE"
+
